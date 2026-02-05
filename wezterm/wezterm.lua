bfdce885c9fa543ee603eb4e3ea930f91bb40793
HarumaIto-loovic <haruma.ito@loovic.co.jp>
2026-02-05 15:43:38 +0900

    add: より使いやすように見た目を改善

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 
 local config = {}
-local opacity = 0.7
+local opacity = 0.8
 local transparent = true
 
 if wezterm.config_builder then
@@ -10,9 +10,28 @@ end
 
 -- カラースキームの設定
 config.color_scheme = "GitHub Dark"
-
+config.font_size = 12
 config.window_background_opacity = opacity
+config.use_ime = true
+config.window_decorations = "RESIZE"
 
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	local background = "#5c6d74"
+	local foreground = "#FFFFFF"
+
+	if tab.is_active then
+		background = "#ae8b2d"
+		foreground = "#FFFFFF"
+	end
+
+	local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. " "
+
+	return {
+		{ Background = { Color = background } },
+		{ Foreground = { Color = foreground } },
+		{ Text = title },
+	}
+end)
 -- 最初からフルスクリーンで起動
 local mux = wezterm.mux
 wezterm.on("gui-startup", function(cmd)
